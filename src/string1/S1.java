1e1e08362c51d1aa648afb102f43b380d6bf3eaf
MY LENOVO <MY LENOVO@DESKTOP-FOREJEP>
2020-05-29 13:02:45 +0530

    accept a String from User and Display it Back on Screen .

---
+++ src/string1/S1.java
@@ -0,0 +1,21 @@
+
+package string1;
+
+import java.util.Scanner;
+
+public class S1 {
+     public static void main(String[] args)
+    {
+        Scanner sc =  new Scanner(System.in);
+        
+        String name,name1;
+        
+        System.out.println("Enter ur name....");
+        
+        //name = sc.next();//Single String
+          name1 = sc.nextLine();//multi sentence 
+        
+        //System.out.println("The Name is:  " + name);
+        System.out.println("The Name is..............:  " + name1);
+    }
+}
